ce62f74560e74165a47f2a42956d62965adc4524
adam <adam.t61vnuhus@gmail.com>
2021-02-22 11:02:10 +0700

    update project task, delete, update validation pt

--- PPMToolFullStack/src/main/java/com/adamvnuhus/ppmtool/controller/BacklogController.java
+++ PPMToolFullStack/src/main/java/com/adamvnuhus/ppmtool/controller/BacklogController.java
@@ -1,9 +1,76 @@
-package com.adamvnuhus.ppmtool.controller;/** 
-  * ppmtool
-  *
-  * @author anhdt
-  * @created_at 19/02/2021 - 5:36 PM
-  * @created_by anhdt
-  * @since 19/02/2021
- */ public class BacklogController {
+package com.adamvnuhus.ppmtool.controller;
+
+import com.adamvnuhus.ppmtool.domain.ProjectTask;
+import com.adamvnuhus.ppmtool.services.MapValidationErrorService;
+import com.adamvnuhus.ppmtool.services.ProjectTaskService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+
+/**
+ * ppmtool
+ *
+ * @author anhdt
+ * @created_at 19/02/2021 - 5:36 PM
+ * @created_by anhdt
+ * @since 19/02/2021
+ */
+@RestController
+@RequestMapping("/api/backlog")
+@CrossOrigin
+public class BacklogController {
+
+    @Autowired
+    private ProjectTaskService projectTaskService;
+
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
+    @PostMapping("/{backlog_id}")
+    public ResponseEntity<?> addPTtoBacklog(@Valid @RequestBody ProjectTask projectTask,
+                                                                        BindingResult result, @PathVariable String backlog_id) {
+         //show delete
+        //custom exception
+
+        ResponseEntity<?> erroMap = mapValidationErrorService.MapValidationService(result);
+        if (erroMap != null) return erroMap;
+
+        ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id, projectTask);
+
+        return new ResponseEntity<ProjectTask>(projectTask1, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/{backlog_id}")
+    public Iterable<ProjectTask> getProjectBacklog(@PathVariable String backlog_id) {
+
+        return projectTaskService.findBacklogById(backlog_id);
+    }
+
+    @GetMapping("/{backlog_id}/{pt_id}")
+    public ResponseEntity<?> getProjectTask(@PathVariable String backlog_id, @PathVariable String pt_id){
+        ProjectTask projectTask = projectTaskService.findPTByProjectSequence(backlog_id, pt_id);
+        return  new ResponseEntity<ProjectTask>(projectTask, HttpStatus.OK);
+    }
+
+    @PatchMapping("/{backlog_id}/{pt_id}")
+    public ResponseEntity<?> updateProjectTask(@Valid @RequestBody ProjectTask projectTask, BindingResult result,
+                                                                        @PathVariable String backlog_id, @PathVariable String pt_id){
+        ResponseEntity<?> erroMap = mapValidationErrorService.MapValidationService(result);
+        if (erroMap != null) return erroMap;
+
+        ProjectTask updatedTask = projectTaskService.updateByProjectSequence(projectTask, backlog_id, pt_id);
+
+        return new ResponseEntity<ProjectTask>(updatedTask, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{backlog_id}/{pt_id}")
+    public ResponseEntity<?> deleteProjectTask(@PathVariable String backlog_id, @PathVariable String pt_id) {
+        projectTaskService.deletePTByProjectSequence(backlog_id, pt_id);
+
+        return new ResponseEntity<String>("Project Task " + pt_id + " was deleted successfully", HttpStatus.OK);
+    }
 }
