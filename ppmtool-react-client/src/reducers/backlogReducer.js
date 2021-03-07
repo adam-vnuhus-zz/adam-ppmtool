818e4c0f7143358e52c24b79c6f16c9791796232
adam <adam.t61vnuhus@gmail.com>
2021-03-07 17:12:34 +0700

    delete project task

--- ppmtool-react-client/src/reducers/backlogReducer.js
+++ ppmtool-react-client/src/reducers/backlogReducer.js
@@ -28,6 +28,8 @@ export default function (state = initialSate, action) {
         ...state,
 
         // TO_DO
+        project_tasks: state.project_tasks.filter(
+          project_task => project_task.projectSequence !== action.payload)
       };
 
     default:
